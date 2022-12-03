6d1268bb58a1db426f0085a5430e1c1fb3907bb8
Ahmed Lafeef EN <lafeefen@gmail.com>
2022-12-03 14:33:16 +0530

    Pizza order ready2

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,13 +5,13 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private boolean[] CheeseToppingsBagBill = new boolean[4];
+    private int[] CheeseToppingsBagBill = new int[4];
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        for(int i=0;i<4;i++) {this.CheeseToppingsBagBill[i] = false;}
-        if(isVeg) this.price = 300;
+        for(int i=0;i<4;i++) {this.CheeseToppingsBagBill[i] = 0;}
+        if(this.isVeg) this.price = 300;
         else this.price = 400;
         this.bill = "Base Price Of The Pizza: "+this.price+"\n";
     }
@@ -22,43 +22,42 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if(!CheeseToppingsBagBill[0]) {
-            this.price += 80;
-            this.bill += "Extra Cheese Added: 80\n";
-        }
-        CheeseToppingsBagBill[0] = true;
+        this.CheeseToppingsBagBill[0]++;
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(!CheeseToppingsBagBill[1]) {
-            if (isVeg) {
-                this.price += 70;
-                this.bill += "Extra Toppings Added: 70\n";
-            } else {
-                this.price += 120;
-                this.bill += "Extra Toppings Added: 120\n";
-            }
-        }
-        CheeseToppingsBagBill[1] = true;
+        this.CheeseToppingsBagBill[1]++;
     }
 
     public void addTakeaway(){
         // your code goes here
-        if(!CheeseToppingsBagBill[2]) {
-            this.price += 20;
-            this.bill += "Paperbag Added: 20\n";
-        }
-        CheeseToppingsBagBill[2] = true;
+        this.CheeseToppingsBagBill[2]++;
     }
 
     public String getBill(){
         // your code goes here
-        if(!CheeseToppingsBagBill[3]) {
-            this.bill += "Total Price: " + this.price + "\n";
-            CheeseToppingsBagBill[3] = true;
-            return this.bill;
+        if(CheeseToppingsBagBill[0]!=0) {
+            this.price += 80*CheeseToppingsBagBill[0];
+            this.bill += "Extra Cheese Added: "+80*CheeseToppingsBagBill[0]+"\n";
         }
-        return "";
+        if(CheeseToppingsBagBill[1]!=0) {
+            if (this.isVeg) {
+                this.price += 70*CheeseToppingsBagBill[1];
+                this.bill += "Extra Toppings Added: "+70*CheeseToppingsBagBill[1]+"\n";
+            } else {
+                this.price += 120*CheeseToppingsBagBill[1];
+                this.bill += "Extra Toppings Added: "+120*CheeseToppingsBagBill[1]+"\n";
+            }
+        }
+        if(CheeseToppingsBagBill[2]!=0) {
+            this.price += 20;
+            this.bill += "Paperbag Added: 20\n";
+        }
+        if(CheeseToppingsBagBill[3]==0) {
+            this.bill += "Total Price: " + getPrice() + "\n";
+            CheeseToppingsBagBill[3]++;
+            return this.bill;
+        }else return "";
     }
 }
